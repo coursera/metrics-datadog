36a0018c2050640a046ad53eb1271990f0e98801
Chris Huang <chris.huang@bazaarvoice.com>
2014-06-04 11:44:02 -0500

    Updated metrics-core to 3.0.1 and jackson-core to 2.3.2
    Follows same pattern as
    https://github.com/dropwizard/metrics/blob/v3.0.1/metrics-graphite/src/main/java/com/codahale/metrics/graphite/GraphiteReporter.java
    Refactored DatadogReporter for metrics-core 3.0.1
    Refactored MetricNameFormatter and DefaultMetricNameFormatter
    Updated/Refactored DatadogReporterTest
    Added Datadog class for creating JSON/posting http request
    Updated README

--- src/main/java/com/codahale/metrics/datadog/model/DatadogGauge.java
+++ src/main/java/com/codahale/metrics/datadog/model/DatadogGauge.java
@@ -1,7 +1,7 @@
 package com.codahale.metrics.datadog.model;
 
-
 public class DatadogGauge extends DatadogSeries<Number> {
+
   public DatadogGauge(String name, Number count, Long epoch, String host) {
     super(name, count, epoch, host);
   }
