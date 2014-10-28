d82ac10b1b1b6432fd439ed637f9a141b938ad32
Claes Bergsten <claes.bergsten@schibstedpayment.no>
2014-10-28 23:03:12 +0100

    Added Dropwizard support for UDP

--- dropwizard-metrics-datadog/src/main/java/io/dropwizard/metrics/DatadogReporterFactory.java
+++ dropwizard-metrics-datadog/src/main/java/io/dropwizard/metrics/DatadogReporterFactory.java
@@ -6,17 +6,27 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonTypeName;
 import org.coursera.metrics.datadog.DatadogReporter;
 import org.coursera.metrics.datadog.transport.HttpTransport;
+import org.coursera.metrics.datadog.transport.Transport;
+import org.coursera.metrics.datadog.transport.UdpTransport;
 
+import javax.validation.constraints.AssertTrue;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import java.util.List;
 
 @JsonTypeName("datadog")
 public class DatadogReporterFactory extends BaseReporterFactory {
+  private final String TRANSPORT_UDP = "udp";
+  private final String TRANSPORT_HTTP = "http";
+
   @NotNull
+  @Pattern(regexp = "http|udp", flags = Pattern.Flag.CASE_INSENSITIVE)
+  @JsonProperty
+  private String transportType = null;
+
   @JsonProperty
   private String host = null;
 
-  @NotNull
   @JsonProperty
   private String apiKey = null;
 
@@ -29,20 +39,40 @@ public class DatadogReporterFactory extends BaseReporterFactory {
   @JsonProperty
   private int socketTimeout = 5000;   // in milliseconds
 
+  @AssertTrue(message = "When using HTTP protocol you must provide host and apiKey")
+  private boolean isValidConfig() {
+      if (TRANSPORT_HTTP.equals(transportType)) {
+          if (host == null || apiKey == null) {
+              return false;
+          }
+      }
+
+      return true;
+  }
+
   public ScheduledReporter build(MetricRegistry registry) {
-    HttpTransport transport = new HttpTransport.Builder()
-        .withApiKey(apiKey)
-        .withConnectTimeout(connectTimeout)
-        .withSocketTimeout(socketTimeout)
-        .build();
+    Transport transport;
+
+    if (TRANSPORT_UDP.equals(transportType)) {
+        transport = new UdpTransport.Builder()
+                .build();
+    } else if (TRANSPORT_HTTP.equals(transportType)) {
+        transport = new HttpTransport.Builder()
+                .withApiKey(apiKey)
+                .withConnectTimeout(connectTimeout)
+                .withSocketTimeout(socketTimeout)
+                .build();
+    } else {
+        throw new IllegalArgumentException("Invalid transport type provided");
+    }
 
     return DatadogReporter.forRegistry(registry)
-        .withTransport(transport)
-        .withHost(host)
-        .withTags(tags)
-        .filter(getFilter())
-        .convertDurationsTo(getDurationUnit())
-        .convertRatesTo(getRateUnit())
-        .build();
-    }
+      .withTransport(transport)
+      .withHost(host)
+      .withTags(tags)
+      .filter(getFilter())
+      .convertDurationsTo(getDurationUnit())
+      .convertRatesTo(getRateUnit())
+      .build();
+  }
 }
